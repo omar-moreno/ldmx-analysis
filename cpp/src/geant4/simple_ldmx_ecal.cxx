676a2134a170a5802fbe19c7b4dd1fde64bb7f83
Omar Moreno <omoreno1@ucsc.edu>
2016-07-15 16:43:44 -0700

    Skeleton for simple Geant4 simulation of the LDMX Ecal.

---
+++ cpp/src/geant4/simple_ldmx_ecal.cxx
@@ -0,0 +1,15 @@
+/**
+ *
+ */
+
+//------------//
+//   Geant4   //
+//------------//
+#include "G4RunManager.hh"
+
+int main(int argc, char** argv) { 
+
+    // Construct the default run manager
+    G4RunManager* run_manager = new G4RunManager(); 
+
+}
