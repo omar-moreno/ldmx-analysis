96b975c0ef19b912e886c7e01cf770495b5d67ce
Omar Moreno <omoreno1@ucsc.edu>
2016-08-12 17:14:33 -0700

    Analysis used to study the performance of the recoil tracker.

---
+++ cpp/include/analyses/RecoilTrackerAnalysis.h
@@ -0,0 +1,70 @@
+/**
+ *	@file RecoilTrackerAnalysis.h
+ *	@brief Analysis used to study the performance of the Recoil tracker.
+ *	@author <a href="mailto:omoreno@slac.stanford.edu">Omar Moreno</a>
+ *	        SLAC National Accelerator Facility
+ *  @date June 27, 2016
+ */
+
+#ifndef __RECOIL_TRACKER_ANALYSIS_H__
+#define __RECOIL_TRACKER_ANALYSIS_H__
+
+//--------------//
+//   Analyses   //
+//--------------//
+#include <LcioAbstractAnalysis.h>
+#include <FlatTupleMaker.h>
+#include <TrackUtils.h>
+
+//----------//
+//   LCIO   //
+//----------//
+#include <EVENT/LCEvent.h>
+#include <EVENT/LCCollection.h>
+#include <EVENT/SimTrackerHit.h>
+#include <EVENT/TrackerRawData.h>
+#include <EVENT/MCParticle.h>
+#include <UTIL/CellIDDecoder.h>
+#include <UTIL/LCRelationNavigator.h>
+
+class RecoilTrackerAnalysis : public LcioAbstractAnalysis { 
+
+    public: 
+
+        /** Default constructor */
+        RecoilTrackerAnalysis();
+
+        /** Destructor */
+        ~RecoilTrackerAnalysis();
+
+		/**
+		 * Method used to initialize an analysis.  This method is called once
+         * before any events are processed. 
+		 */
+		void initialize(); 
+
+        /**
+         *  Method used to process an event (LCEvent).  This method is called 
+         *  once for every event. 
+         *
+         *  @param event The event to process.
+         */
+        void processEvent(EVENT::LCEvent* event);
+
+        /** 
+         * Method called at the end of an analysis.  This method is called once
+         * per run.
+         */
+        void finalize();
+
+    private: 
+
+        /** Allows the creation of a ROOT ntuple */
+        FlatTupleMaker* tuple;
+
+        /** Findable track count */
+        //int findable_track;
+
+}; // RecoilTrackerAnalysis
+
+#endif // __RECOIL_TRACKER_ANALYSIS_H__
---
+++ cpp/src/analyses/RecoilTrackerAnalysis.cxx
@@ -0,0 +1,238 @@
+/**
+ *	@file RecoilTrackerAnalysis.cxx
+ *	@brief Analysis used to study the performance of the Recoil tracker.
+ *	@author <a href="mailto:omoreno@slac.stanford.edu">Omar Moreno</a>
+ *	        SLAC National Accelerator Facility
+ *  @date June 27, 2016
+ */
+
+#include <RecoilTrackerAnalysis.h>
+
+RecoilTrackerAnalysis::RecoilTrackerAnalysis() 
+    : tuple(new FlatTupleMaker("recoil_tracker_tuple_test.root", "results")) { 
+      //findable_track(0) {
+    LcioAbstractAnalysis::class_name = "RecoilTrackerAnalysis";
+}
+
+RecoilTrackerAnalysis::~RecoilTrackerAnalysis() { 
+}
+
+void RecoilTrackerAnalysis::initialize() {
+ 
+    // Event information 
+    tuple->addVariable("event");
+    tuple->addVariable("n_tracks");
+
+    // Track parameters and kinematics
+    tuple->addVariable("n_track_hits");
+
+    tuple->addVariable("chi2"); 
+    tuple->addVariable("d0");
+    tuple->addVariable("omega");
+    tuple->addVariable("phi0");
+    tuple->addVariable("tan_lambda");
+    tuple->addVariable("z0");
+    
+    tuple->addVariable("p");
+    tuple->addVariable("pt");
+    tuple->addVariable("px");
+    tuple->addVariable("py");
+    tuple->addVariable("pz");
+
+    /*tuple->addVariable("beam_e_p");  
+    tuple->addVariable("beam_e_px");  
+    tuple->addVariable("beam_e_py");  
+    tuple->addVariable("beam_e_pz");  
+    tuple->addVariable("beam_e_p_last");  
+    tuple->addVariable("beam_e_px_last");  
+    tuple->addVariable("beam_e_py_last");  
+    tuple->addVariable("beam_e_pz_last");  
+    tuple->addVariable("n_mishits"); 
+    tuple->addVariable("is_findable");
+    tuple->addVariable("x_target");
+    tuple->addVariable("y_target");
+
+    tuple->addVector("sim_hit_layer");
+    tuple->addVector("sim_hit_pos_x");
+    tuple->addVector("sim_hit_pos_y");
+    tuple->addVector("sim_hit_pos_z");
+    tuple->addVector("sim_hit_time");*/
+}
+
+void RecoilTrackerAnalysis::processEvent(EVENT::LCEvent* event) { 
+   
+    tuple->setVariableValue("event", event->getEventNumber());
+    // Get the collection of MC particles from the event. If no such collection
+    // exist, a DataNotAvailableException is thrown.
+    //EVENT::LCCollection* mc_particles 
+    //   = (EVENT::LCCollection*) event->getCollection("MCParticle");
+   
+    // Create a pointer to the incident electron i.e. the beam electron. 
+    //EVENT::MCParticle* beam_e = nullptr;
+
+    // Loop over all of the MC particles and find the beam electron. For now, 
+    // this is simply done by looking for an electron that has no parent. 
+    //int n_electrons = 0;
+    //double largest_brem_energy = 0; 
+    //double brem_position = -1; 
+    //for (int mc_particle_n = 0; mc_particle_n < mc_particles->getNumberOfElements(); ++mc_particle_n) {
+
+    //    EVENT::MCParticle* particle = (EVENT::MCParticle*) mc_particles->getElementAt(mc_particle_n);
+    
+        //if (particle->getPDG() == 11) n_electrons++; 
+
+    //    if (particle->getParents().size() == 0) {
+    //       beam_e = particle; 
+    //    }
+
+       /*if (particle->getPDG() == 22 && particle->getVertex()[2] < -3) { 
+            double* brem_energy_vec = (double*) particle->getMomentum(); 
+            double brem_energy = sqrt(pow(brem_energy_vec[0], 2) + pow(brem_energy_vec[1], 2) + pow(brem_energy_vec[2], 2));
+            if (brem_energy > largest_brem_energy) {
+                largest_brem_energy = brem_energy;
+                brem_position = particle->getVertex()[2];
+            }
+       }*/ 
+    //}
+
+    // Calculate the momentum of the incident electron
+    /*double* beam_e_pvec = (double*) beam_e->getMomentum();
+    double beam_e_p = sqrt(pow(beam_e_pvec[0], 2) + pow(beam_e_pvec[1], 2) + pow(beam_e_pvec[2], 2));
+    tuple->setVariableValue("beam_e_p", beam_e_p); 
+    tuple->setVariableValue("beam_e_px", beam_e_pvec[0]); 
+    tuple->setVariableValue("beam_e_py", beam_e_pvec[1]); 
+    tuple->setVariableValue("beam_e_pz", beam_e_pvec[2]);
+    
+
+    // Get the collection of SimTrackerHits associated with the Recoil tracker
+    // from the event.  If no such collection exist, a DataNotAvailableException
+    // is thrown.
+    EVENT::LCCollection* sim_hits 
+        = (EVENT::LCCollection*) event->getCollection("RecoilTrackerHits");
+
+    // Create a cell ID decoder used to get specific properties associated with
+    // Recoil SimTrackerHits i.e. layer.
+    UTIL::CellIDDecoder<EVENT::SimTrackerHit> sim_hit_decoder(sim_hits);
+    */
+
+    /*for (int sim_hit_n = 0; sim_hit_n < sim_hits->getNumberOfElements(); ++sim_hit_n) { 
+        
+        // Get a Recoil SimTrackerHit from the collection of hits.
+        EVENT::SimTrackerHit* sim_hit 
+            = (EVENT::SimTrackerHit*) sim_hits->getElementAt(sim_hit_n); 
+
+        // Get the layer number associated with this hit.
+        int layer = sim_hit_decoder(sim_hit)["layer"];
+
+        tuple->addToVector("sim_hit_layer", layer);
+        tuple->addToVector("sim_hit_pos_x", sim_hit->getPosition()[0]); 
+        tuple->addToVector("sim_hit_pos_y", sim_hit->getPosition()[1]); 
+        tuple->addToVector("sim_hit_pos_z", sim_hit->getPosition()[2]); 
+        tuple->addToVector("sim_hit_time", sim_hit->getTime());
+    }*/
+
+    // Check if the track is findable
+    /*tuple->setVariableValue("is_findable", 0);
+    std::vector<EVENT::SimTrackerHit*> findable_sim_hits; 
+    if (TrackUtils::isTrackFindable(14, beam_e, sim_hits, findable_sim_hits)) {
+        ++findable_track;
+        tuple->setVariableValue("is_findable", 1);  
+    }*/
+    
+    /*for (EVENT::SimTrackerHit* sim_hit : findable_sim_hits) { 
+        
+        // Get the layer number associated with this hit.
+        int layer = sim_hit_decoder(sim_hit)["layer"];
+
+        tuple->addToVector("sim_hit_layer", layer);
+        tuple->addToVector("sim_hit_pos_x", sim_hit->getPosition()[0]); 
+        tuple->addToVector("sim_hit_pos_y", sim_hit->getPosition()[1]); 
+        tuple->addToVector("sim_hit_pos_z", sim_hit->getPosition()[2]); 
+        tuple->addToVector("sim_hit_time", sim_hit->getTime());
+    
+        if ((beam_e == sim_hit->getMCParticle()) && (layer == 14)) { 
+    
+            // Calculate the momentum of the incident electron
+            float* sim_hit_pvec = (float*) sim_hit->getMomentum();
+            double sim_hit_p = sqrt(pow(sim_hit_pvec[0], 2) + pow(sim_hit_pvec[1], 2) + pow(sim_hit_pvec[2], 2));
+            tuple->setVariableValue("beam_e_p_last", sim_hit_p); 
+            tuple->setVariableValue("beam_e_px_last", sim_hit_pvec[0]); 
+            tuple->setVariableValue("beam_e_py_last", sim_hit_pvec[1]); 
+            tuple->setVariableValue("beam_e_pz_last", sim_hit_pvec[2]); 
+        }
+    }
+
+    // Get the collection of LCRelations between a raw hit and a SimTrackerHit.
+    EVENT::LCCollection* true_hit_relations
+        = (EVENT::LCCollection*) event->getCollection("RecoilTrueHitRelations");
+
+    // Instantiate an LCRelation navigator which allows faster access to either
+    // the SimTrackerHit or raw hits.
+    UTIL::LCRelationNavigator* true_hit_relations_nav
+        = new UTIL::LCRelationNavigator(true_hit_relations);
+    */
+    // Get the collection of Recoil tracker tracks from the event.  If no such
+    // collection exist, a DataNotAvailableException is thrown.
+    EVENT::LCCollection* tracks 
+        = (EVENT::LCCollection*) event->getCollection("RecoilTracks");
+   
+    tuple->setVariableValue("n_tracks", tracks->getNumberOfElements());
+    if (tracks->getNumberOfElements() == 0) { 
+        tuple->fill();
+        tuple->clear();
+    }
+   
+    for (int track_n = 0; track_n < tracks->getNumberOfElements(); ++track_n) {
+
+        EVENT::Track* track = (EVENT::Track*) tracks->getElementAt(track_n); 
+    
+        std::vector<TrackerHit*> track_hits = track->getTrackerHits();
+        /*
+
+        double n_mishit = 0;  
+        for (TrackerHit* track_hit : track_hits) {
+            bool mishit_found = false;
+            for (int raw_hit_n = 0; raw_hit_n < track_hit->getRawHits().size(); ++raw_hit_n) { 
+                
+                EVENT::TrackerRawData* tagger_track_raw_hit 
+                    = (EVENT::TrackerRawData*) track_hit->getRawHits()[raw_hit_n];
+                
+                EVENT::LCObjectVec sim_hit_list 
+                    = true_hit_relations_nav->getRelatedToObjects(tagger_track_raw_hit);
+                
+                if (((EVENT::SimTrackerHit*) sim_hit_list[0])->getMCParticle() != beam_e) { 
+                    mishit_found = true;
+                }
+            }
+            if (mishit_found) n_mishit++; 
+        } 
+        tuple->setVariableValue("n_mishits", n_mishit); */
+
+        double p = TrackUtils::getMomentum(track, -0.75);
+        std::vector<double> p_vec = TrackUtils::getMomentumVector(track, -1.5);
+        double pt = sqrt(p_vec[1]*p_vec[1] + p_vec[2]*p_vec[2]); 
+
+        tuple->setVariableValue("n_track_hits", track_hits.size());
+
+        tuple->setVariableValue("chi2", track->getChi2());
+        tuple->setVariableValue("p", p);
+        tuple->setVariableValue("pt", pt);
+        tuple->setVariableValue("px", p_vec[0]);
+        tuple->setVariableValue("py", p_vec[1]);
+        tuple->setVariableValue("pz", p_vec[2]);
+        tuple->setVariableValue("d0", track->getD0());
+        tuple->setVariableValue("z0", track->getZ0());
+        tuple->setVariableValue("omega", track->getOmega());
+        tuple->setVariableValue("phi0", track->getPhi());
+        tuple->setVariableValue("tan_lambda", track->getTanLambda());
+        
+        tuple->fill(); 
+    }
+
+    tuple->clear();
+
+}
+
+void RecoilTrackerAnalysis::finalize() { 
+    tuple->close();
+}
