73586e783366dfb019df4d7b43cdc77b8d42d5dd
Omar Moreno <omoreno1@ucsc.edu>
2016-09-07 16:19:57 -0700

    Added Tagger tracker sim hit info to ntuple.  Added flags indicating whether a recoil track is findable.

--- cpp/src/analyses/PhotoNuclearAnalysis.cxx
+++ cpp/src/analyses/PhotoNuclearAnalysis.cxx
@@ -24,16 +24,22 @@ void PhotoNuclearAnalysis::initialize() {
       plot->GetXaxis()->SetTitle("Layer number"); 
       plot->GetYaxis()->SetTitle("Total hits");*/
 
+    tuple->addVariable("event");
+
+    tuple->addVariable("recoil_is_findable");
+    tuple->addVariable("recoil_energy");
+
     tuple->addVariable("pn_gamma_energy");
     tuple->addVariable("pn_particle_mult");
+
     tuple->addVariable("n_recoil_hits");
-    tuple->addVariable("n_neutron");
-    tuple->addVariable("n_pi0");
-    tuple->addVariable("n_pip");
-    tuple->addVariable("n_pim");
-    tuple->addVariable("n_proton");
-    tuple->addVariable("n_ion");
-    tuple->addVariable("n_gamma");
+    tuple->addVariable("n_recoil_pn_hits");
+    tuple->addVariable("n_recoil_tracks");
+
+    tuple->addVector("pn_pdg_id");
+    tuple->addVector("pn_theta");
+    tuple->addVector("pn_phi");
+    tuple->addVector("is_findable");
 
     tuple->addVector("recoil_sim_hit_layer");
     tuple->addVector("recoil_sim_hit_dedx");
@@ -42,12 +48,27 @@ void PhotoNuclearAnalysis::initialize() {
     tuple->addVector("recoil_sim_hit_pos_x");
     tuple->addVector("recoil_sim_hit_pos_y");
     tuple->addVector("recoil_sim_hit_pos_z");
+
+    tuple->addVector("tagger_sim_hit_layer");
+    tuple->addVector("tagger_sim_hit_dedx");
+    tuple->addVector("tagger_sim_hit_backscattered");
+    tuple->addVector("tagger_sim_hit_time");
+    tuple->addVector("tagger_sim_hit_pos_x");
+    tuple->addVector("tagger_sim_hit_pos_y");
+    tuple->addVector("tagger_sim_hit_pos_z");
 }
 
 void PhotoNuclearAnalysis::processEvent(EVENT::LCEvent* event) { 
 
     tuple->setVariableValue("event", event->getEventNumber());
-    //std::cout << "Event: " << event->getEventNumber() << std::endl;
+    std::cout << "Event: " << event->getEventNumber() << std::endl;
+
+    // Get the collection of SimTrackerHits associated with the Tagger tracker
+    // from the event.  If no such collection exist, a DataNotAvailableException
+    // is thrown.
+    EVENT::LCCollection* recoil_sim_hits 
+        = (EVENT::LCCollection*) event->getCollection("RecoilTrackerHits");
+
 
     // Get the collection of MC particles from the event. If no such collection
     // exist, a DataNotAvailableException is thrown.
@@ -59,6 +80,7 @@ void PhotoNuclearAnalysis::processEvent(EVENT::LCEvent* event) {
     // Loop over all of the MC particles and find the beam electron. For now, 
     // this is simply done by looking for an electron that has no parent. 
     EVENT::MCParticle* pn_gamma = nullptr;
+    EVENT::MCParticle* e_recoil = nullptr;
     double n_neutron = 0;
     double n_pi0 = 0; 
     double n_pip = 0;
@@ -67,49 +89,66 @@ void PhotoNuclearAnalysis::processEvent(EVENT::LCEvent* event) {
     double n_ion = 0;
     double n_gamma = 0;
 
+    int first_strategy[10] = { 1, 1, 1, 1, 1, 1, 0, 0, 1, 0 }; 
+    int second_strategy[10] = { 1, 1, 1, 1, 1, 1, 0, 0, 0, 1 };
+    int total_recoil_tracks = 0; 
     for (int mc_particle_n = 0; mc_particle_n < mc_particles->getNumberOfElements(); ++mc_particle_n) {
 
         EVENT::MCParticle* mc_particle = (EVENT::MCParticle*) mc_particles->getElementAt(mc_particle_n);
-        //std::cout << "MC Particle PDG ID: " << mc_particle->getPDG() << " vertex_z : " << mc_particle->getVertex()[2] << std::endl;
+       
+        if (mc_particle->getPDG() == 11 && mc_particle->getParents().size() == 0) { 
+            
+            e_recoil = mc_particle;
+
+            tuple->setVariableValue("recoil_is_findable", 0); 
+            if (TrackUtils::isTrackFindable(10, first_strategy, mc_particle, recoil_sim_hits)
+                    || TrackUtils::isTrackFindable(10, second_strategy, mc_particle, recoil_sim_hits)) {
+                //std::cout << "Particle (PDG ID = " << daughter->getPDG() << ") is findable." << std::endl;
+                tuple->setVariableValue("recoil_is_findable", 1);
+                total_recoil_tracks++; 
+            } 
+        }
+        
+        std::cout << "MC Particle PDG ID: " << mc_particle->getPDG() << " vertex_z : " << mc_particle->getVertex()[2] << std::endl;
         if (mc_particle->getPDG() == 22 && createdWithinTarget(mc_particle)) {
+            
+            std::cout << "Found particle created within target." << std::endl;
+            if (mc_particle->getParents().size() == 0) std::cout << "null" << std::endl;
             if (mc_particle->getParents()[0]->getParents().size() == 0) { 
     
-                //std::cout << "Photonuclear gamma found." << std::endl;
+                std::cout << "Photonuclear gamma found." << std::endl;
 
                 tuple->setVariableValue("pn_gamma_energy", mc_particle->getEnergy());
                 tuple->setVariableValue("pn_particle_mult", mc_particle->getDaughters().size());
 
                 pn_gamma = mc_particle;
 
-                for (auto daughter : mc_particle->getDaughters()) { 
-                    switch (daughter->getPDG()) { 
-                        case 2112 : 
-                            n_neutron++;
-                            break;
-                        case 2212 : 
-                            n_proton++;
-                            break;
-                        case 111: 
-                            n_pi0++;
-                            break;
-                        case 211: 
-                            n_pip++;
-                            break;
-                        case -211: 
-                            n_pim++;
-                            break;
-                        case 22: 
-                            n_gamma; 
-                            break;
-                        default : 
-                            n_ion++;
-                            break;
-                    }
-                }
+                /*
+                for (auto daughter : mc_particle->getDaughters()) {
+
+                    // Check if the track is findable
+                    tuple->addToVector("pn_pdg_id", daughter->getPDG());
+
+                    double* pvec = (double*) daughter->getMomentum();
+                    double p = sqrt(pow(pvec[0], 2) + pow(pvec[1], 2) + pow(pvec[2], 2));
+                    double theta = acos(pvec[2]/p)*180/3.14159;
+                    tuple->addToVector("pn_theta", theta);
+
+                    int is_findable = 0;
+                    if (daughter->getCharge() != 0) {
+                        if (TrackUtils::isTrackFindable(10, first_strategy, daughter, recoil_sim_hits)
+                               || TrackUtils::isTrackFindable(10, second_strategy, daughter, recoil_sim_hits)) {
+                            //std::cout << "Particle (PDG ID = " << daughter->getPDG() << ") is findable." << std::endl;
+                            is_findable = 1;
+                            total_recoil_tracks++;
+                        } 
+                    } 
+                    tuple->addToVector("is_findable", is_findable);
+                }*/
             }
         }
 
-
+        /*
         tuple->setVariableValue("n_neutron", n_neutron);
         tuple->setVariableValue("n_pi0", n_pi0);
         tuple->setVariableValue("n_pip", n_pip);
@@ -120,44 +159,80 @@ void PhotoNuclearAnalysis::processEvent(EVENT::LCEvent* event) {
 
         if (mc_particle->getPDG() > 100 && !createdWithinTarget(mc_particle)) {
             //std::cout << "Skipping event with photo-nuclear event downstream." << std::endl;
+            tuple->fill();
             return;
-        }
+        }*/
     }
+    
+    /*
+    tuple->setVariableValue("n_recoil_tracks", total_recoil_tracks);
 
     // Get the collection of SimTrackerHits associated with the Tagger tracker
     // from the event.  If no such collection exist, a DataNotAvailableException
     // is thrown.
-    EVENT::LCCollection* sim_hits 
-        = (EVENT::LCCollection*) event->getCollection("RecoilTrackerHits");
+    EVENT::LCCollection* tagger_sim_hits 
+        = (EVENT::LCCollection*) event->getCollection("TaggerTrackerHits");
 
     // Create a cell ID decoder used to get specific properties associated with
     // Tagger SimTrackerHits i.e. layer.
-    UTIL::CellIDDecoder<EVENT::SimTrackerHit> sim_hit_decoder(sim_hits);
-    tuple->setVariableValue("n_recoil_hits", sim_hits->getNumberOfElements());
+    UTIL::CellIDDecoder<EVENT::SimTrackerHit> tagger_sim_hit_decoder(tagger_sim_hits);
 
-    std::vector<int> sim_hits_vec(10, 0);
-    for (int sim_hit_n = 0; sim_hit_n < sim_hits->getNumberOfElements(); ++sim_hit_n) { 
-
+    std::vector<int> tagger_sim_hits_vec(14, 0);
+    for (int sim_hit_n = 0; sim_hit_n < tagger_sim_hits->getNumberOfElements(); ++sim_hit_n) { 
+        
         // Get a Tagger SimTrackerHit from the collection of hits.
-        EVENT::SimTrackerHit* sim_hit = (EVENT::SimTrackerHit*) sim_hits->getElementAt(sim_hit_n); 
+        EVENT::SimTrackerHit* tagger_sim_hit 
+            = (EVENT::SimTrackerHit*) tagger_sim_hits->getElementAt(sim_hit_n); 
 
         // Get the layer number associated with this hit.
-        int layer = sim_hit_decoder(sim_hit)["layer"];
-        sim_hits_vec[layer - 1]++;
+        int layer = tagger_sim_hit_decoder(tagger_sim_hit)["layer"];
+        tagger_sim_hits_vec[layer - 1]++;
 
-        tuple->addToVector("recoil_sim_hit_backscattered", 0);
-        if (sim_hit->getMCParticle()->isBackscatter()) 
-            tuple->addToVector("recoil_sim_hit_backscattered", 1);
-        tuple->addToVector("recoil_sim_hit_layer", layer);
-        tuple->addToVector("recoil_sim_hit_time",  sim_hit->getEDep());
-        tuple->addToVector("recoil_sim_hit_time",  sim_hit->getTime());
-        tuple->addToVector("recoil_sim_hit_pos_x", sim_hit->getPosition()[0]); 
-        tuple->addToVector("recoil_sim_hit_pos_y", sim_hit->getPosition()[1]); 
-        tuple->addToVector("recoil_sim_hit_pos_z", sim_hit->getPosition()[2]); 
+        tuple->addToVector("tagger_sim_hit_backscattered", 0);
+        if (tagger_sim_hit->getMCParticle()->isBackscatter()) 
+            tuple->addToVector("tagger_sim_hit_backscattered", 1);
+        tuple->addToVector("tagger_sim_hit_layer", layer);
+        tuple->addToVector("tagger_sim_hit_dedx",  tagger_sim_hit->getEDep());
+        tuple->addToVector("tagger_sim_hit_time",  tagger_sim_hit->getTime());
+        tuple->addToVector("tagger_sim_hit_pos_x", tagger_sim_hit->getPosition()[0]); 
+        tuple->addToVector("tagger_sim_hit_pos_y", tagger_sim_hit->getPosition()[1]); 
+        tuple->addToVector("tagger_sim_hit_pos_z", tagger_sim_hit->getPosition()[2]); 
+
     }
 
+    // Create a cell ID decoder used to get specific properties associated with
+    // Tagger SimTrackerHits i.e. layer.
+    UTIL::CellIDDecoder<EVENT::SimTrackerHit> recoil_sim_hit_decoder(recoil_sim_hits);
+    tuple->setVariableValue("n_recoil_hits", recoil_sim_hits->getNumberOfElements());
+
+    std::vector<int> recoil_sim_hits_vec(10, 0);
+    int n_recoil_pn_hits = 0;
+    for (int sim_hit_n = 0; sim_hit_n < recoil_sim_hits->getNumberOfElements(); ++sim_hit_n) { 
+
+        // Get a Tagger SimTrackerHit from the collection of hits.
+        EVENT::SimTrackerHit* recoil_sim_hit = (EVENT::SimTrackerHit*) recoil_sim_hits->getElementAt(sim_hit_n); 
+
+        // Get the layer number associated with this hit.
+        int layer = recoil_sim_hit_decoder(recoil_sim_hit)["layer"];
+        recoil_sim_hits_vec[layer - 1]++;
+
+        tuple->addToVector("recoil_sim_hit_backscattered", 0);
+        if (recoil_sim_hit->getMCParticle()->isBackscatter()) 
+            tuple->addToVector("recoil_sim_hit_backscattered", 1);
+        tuple->addToVector("recoil_sim_hit_layer", layer);
+        tuple->addToVector("recoil_sim_hit_dedx",  recoil_sim_hit->getEDep());
+        tuple->addToVector("recoil_sim_hit_time",  recoil_sim_hit->getTime());
+        tuple->addToVector("recoil_sim_hit_pos_x", recoil_sim_hit->getPosition()[0]); 
+        tuple->addToVector("recoil_sim_hit_pos_y", recoil_sim_hit->getPosition()[1]); 
+        tuple->addToVector("recoil_sim_hit_pos_z", recoil_sim_hit->getPosition()[2]); 
+    
+        if (recoil_sim_hit->getMCParticle() != e_recoil) n_recoil_pn_hits++;
+    }
+    tuple->setVariableValue("n_recoil_pn_hits", n_recoil_pn_hits);
+    
+    */
     tuple->fill();
-    tuple->clear();
+    tuple->clear(); 
 }
 
 void PhotoNuclearAnalysis::finalize() { 
